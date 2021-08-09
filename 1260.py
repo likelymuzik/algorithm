889fb4d8cf38a6d240299d79f431b97a901374f0
Hyunu <harry.kim.8918@gmail.com>
2021-08-09 21:10:09 +0900

    first commit

---
+++ 1260.py
@@ -0,0 +1,55 @@
+'''
+문제
+그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 
+단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 
+정점 번호는 1번부터 N번까지이다.
+
+입력
+첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000), 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다. 
+다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. 
+어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 
+입력으로 주어지는 간선은 양방향이다.
+
+출력
+첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. 
+V부터 방문된 점을 순서대로 출력하면 된다.
+'''
+
+from collections import deque
+
+N, M, V = map(int, input().split())
+
+graph = [[] for i in range(N + 1)]
+for _ in range(M):
+    v1, v2 = map(int, input().split())
+    if v2 not in graph[v1]:
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+
+for array in graph:
+    if len(array) != 0:
+        array.sort()
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+visited = [False] * (N + 1)
+dfs(graph, V, visited)
+print()
+visited = [False] * (N + 1)
+bfs(graph, V, visited)
